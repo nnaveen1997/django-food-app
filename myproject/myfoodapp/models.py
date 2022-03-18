1d96b6cdb1045a402387f04cc0539f7895c79bb9
Naveen <hammer19971@gmail.com>
2022-03-18 22:28:48 +0530

    Added kid and image models to models.py

--- myproject/myfoodapp/models.py
+++ myproject/myfoodapp/models.py
@@ -1,3 +1,19 @@
+from statistics import mode
 from django.db import models
 
 # Create your models here.
+class Kid(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=122)
+    age = models.IntegerField()
+    parent_phone = models.CharField(max_length=122)
+    parent_email = models.EmailField(max_length=122)
+
+class Image(models.Model):
+    kid_id = models.ForeignKey(Kid, on_delete=models.DO_NOTHING)
+    img_url = models.CharField(max_length=255)
+    created_on = models.DateField()
+    updated_on = models.DateField()
+    is_approved = models.BooleanField()
+    approved_by = models.CharField(max_length=122)
+    food_group = models.CharField(max_length=122)
