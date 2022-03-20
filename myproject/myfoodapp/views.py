0af544400f28ccbeadc5138a6e78447867b0e2c5
Naveen <hammer19971@gmail.com>
2022-03-20 23:08:03 +0530

    Added index in view

--- myproject/myfoodapp/views.py
+++ myproject/myfoodapp/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-from myfoodapp.models import Image
 
 # Create your views here.
+def index(request):
+    return HttpResponse('Welcome to the Django Food App.')
